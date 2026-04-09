32292f723650ef17c1566285630a20abce4f0f56
Le Roy Yeo <42214749+DiamondTop@users.noreply.github.com>
2026-04-09 11:05:36 -0400

    Update app2.py

--- app2.py
+++ app2.py
@@ -255,7 +255,7 @@ def get_llm_response(history: list, file_context: str, provider: str) -> tuple[s
             model_id = "minimax/minimax-m2.5:free"
         elif "nemotron-3 by Nvidia" in provider:
             model_id = "nvidia/nemotron-3-super-120b-a12b:free"
-        elif "Open AI 120b" in provider:
+        elif "OpenAI" in provider:
             model_id = "openai/gpt-oss-120b:free"
         else:
             model_id = "meta-llama/llama-3.1-8b-instruct:free"
